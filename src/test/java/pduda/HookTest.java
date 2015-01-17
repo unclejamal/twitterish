64232a86015167ce22f840b482c20a30945eee25
unclejamal <unclejamal@gmail.com>
2015-01-17 12:02:04 +0000

    hook test

---
+++ src/test/java/pduda/HookTest.java
@@ -0,0 +1,12 @@
+package pduda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HookTest {
+    @Test
+    public void environmentWorks() {
+        assertEquals(1,1);
+    }
+}
