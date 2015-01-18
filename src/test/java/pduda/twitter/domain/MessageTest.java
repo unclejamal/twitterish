3af346437aaf6b5fa4b518c0befde7146c93f121
unclejamal <unclejamal@gmail.com>
2015-01-18 16:10:33 +0000

    removed duplication

--- src/test/java/pduda/twitter/domain/MessageTest.java
+++ src/test/java/pduda/twitter/domain/MessageTest.java
@@ -1,5 +1,6 @@
 package pduda.twitter.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
@@ -9,18 +10,24 @@ import static org.junit.Assert.*;
 
 public class MessageTest {
 
+    private SocialNetworker networker;
+    private SocialNetworker bob;
+    private Message messagePostedByBob;
+
+    @Before
+    public void setUp() throws Exception {
+        networker = new SocialNetworker("alice");
+        bob = new SocialNetworker("bob");
+        messagePostedByBob = new Message(bob, "content", new Date(1));
+    }
+
     @Test
     public void tellsTheMessageHasBeenPostedBySocialNetworker() {
-        SocialNetworker bob = new SocialNetworker("bob");
-        Message message = new Message(bob, "content", new Date(1));
-        assertThat(message.hasBeenPostedBy(bob), is(true));
+        assertThat(messagePostedByBob.hasBeenPostedBy(bob), is(true));
     }
 
     @Test
     public void tellsTheMessageHasNotBeenPostedByADifferentSocialNetworker() {
-        SocialNetworker alice = new SocialNetworker("alice");
-        SocialNetworker bob = new SocialNetworker("bob");
-        Message message = new Message(bob, "content", new Date(1));
-        assertThat(message.hasBeenPostedBy(alice), is(false));
+        assertThat(messagePostedByBob.hasBeenPostedBy(networker), is(false));
     }
 }
