a6c7aaeaefea7201fc764c20ad3eafd930fc693a
unclejamal <unclejamal@gmail.com>
2015-01-25 12:49:44 +0000

    removed duplication from the view test

--- src/test/java/pduda/twitter/ui/TheViewTest.java
+++ src/test/java/pduda/twitter/ui/TheViewTest.java
@@ -1,5 +1,6 @@
 package pduda.twitter.ui;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import pduda.twitter.domain.Message;
@@ -13,18 +14,23 @@ import static java.util.Collections.emptyList;
 
 public class TheViewTest {
 
+    private ConsoleOutput output;
+    private TheView view;
+
+    @Before
+    public void setUp() throws Exception {
+        output = Mockito.mock(ConsoleOutput.class);
+        view = new TheView(output);
+    }
+
     @Test
     public void doesNotOutputAnythingForAnEmptyTimeline() {
-        ConsoleOutput output = Mockito.mock(ConsoleOutput.class);
-        TheView view = new TheView(output);
         view.present(new Timeline(emptyList()));
         Mockito.verifyZeroInteractions(output);
     }
 
     @Test
     public void outputsMessagesForNonEmptyTimelines() {
-        ConsoleOutput output = Mockito.mock(ConsoleOutput.class);
-        TheView view = new TheView(output);
         view.present(new Timeline(asList(new Message(new SocialNetworker("bob"), "content", new Date(1)))));
         Mockito.verify(output).writeLineAndFlush("content (5 minutes ago)");
     }
