c5aa38db0ab4f5cab368bf5ca8bfdd22489781e2
unclejamal <unclejamal@gmail.com>
2015-02-01 23:52:11 +0000

    minor refactoring

--- src/test/java/pduda/twitter/domain/SocialNetworkerTest.java
+++ src/test/java/pduda/twitter/domain/SocialNetworkerTest.java
@@ -1,11 +1,7 @@
-package pduda.twitter.domain.usecase;
+package pduda.twitter.domain;
 
 import org.junit.Before;
 import org.junit.Test;
-import pduda.twitter.domain.AccountName;
-import pduda.twitter.domain.Message;
-import pduda.twitter.domain.SocialNetworker;
-import pduda.twitter.domain.Timeline;
 
 import java.time.Instant;
 
