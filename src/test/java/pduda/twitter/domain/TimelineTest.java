6537938ea39df0ec97187406090f8b1e610b95ec
unclejamal <unclejamal@gmail.com>
2015-02-09 20:55:51 +0000

    merging two empty timelines

--- src/test/java/pduda/twitter/domain/TimelineTest.java
+++ src/test/java/pduda/twitter/domain/TimelineTest.java
@@ -2,9 +2,13 @@ package pduda.twitter.domain;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class TimelineTest {
     @Test
-    public void emptyTimeline() {
-        Timeline.empty();
+    public void createsAnEmptyTimelineWhenMergingEmptyTimelines() {
+        Timeline resultingTimeline = Timeline.empty().mergeWith(Timeline.empty());
+        assertThat(resultingTimeline, is(Timeline.empty()));
     }
 }
